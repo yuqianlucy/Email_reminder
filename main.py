bb29ab122d1c56884c85c49e1ffb5129b7c138f3
Lucy <133673971+lucyocean@users.noreply.github.com>
2023-07-31 20:09:41 -0400

    keep progressing on senting  email reminder.

--- main.py
+++ main.py
@@ -1,60 +1,150 @@
-# Import the require libirary
+# # Import the require libirary
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 import smtplib
-import schedule
-import time
+import os
 
-# Creating an function to send email, which require three parameter, the to_email, the subject, and the body of the email
-def send_email(to_email, subject, body):
-    # Need to set variable to store which email it is from
-    from_email = 'Your_email@gmail.com'
-    # setting an variable to store the password, kind of worry of security issues, might need to come up a way to fix or modify it
-    password = 'your_password'
-    # setting up the server
-    smtp_server = 'smtp.gmail.com'
-    # setting up the port 
-    smtp_port = 587
+# Step 2: we are setting up an connection to our email server
+smtp = smtplib.SMTP('smtp.gmail.com',587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login('YourMail@gmail.com','Your Password')
 
-    # Setting up an try block to check the server connections
-    try:
-        #Create a secure connection to the SMTP server, using the server variable and port variable
-        server = smtplib.SMTP(smtp_server,smtp_port)
-        # the starttls function does: (keyfile: str | None = None, certfile: str | None = None, context: SSLContext | None = None) -> _ReplyPuts the connection to the SMTP server into TLS mode.
-        #If there has been no previous EHLO or HELO command this session, this method tries ESMTP EHLO first.
-        #If the server supports TLS, this will encrypt the rest of the SMTP session. If you provide the keyfile and certfile parameters, the identity of the SMTP server and client can be checked. This, however, depends on whether the socket module really checks the certificates.
-        #This method may raise the following exceptions:
-        #SMTPHeloError The server didn't reply properly tothe helo greeting.
-        server.starttls()
-        # We are using server.login function to login to the personal email account
-        server.login(from_email, password)
-        # Then, we are composing the email message
-        message = f'Subject: {subject}\n\n{body}'
-        # We are sending the email with three parameter from_email, to_email, message
-        server.sendmail(from_email, to_email, message)
-        # if Email are senting successfully, then we print out an message to show
-        print("Email sent successfully!")
+# Step 3: send the email message 'msg' to the owner
+def message(subject="Python Notification",
+            text="", img=None,
+            attachment=None):
+    # build message contnets
+    msg = MIMEApplication()
+    # we are adding Subject
+    msg['Subject']=subject
+    # Add text contents
+    msg.attach(MIMEText(text))
+    # We are checking if we have anything
+    # given the img parameter
+    if img is not None:
+        # We are Checking whether we have the lists of images or not!
+        if type(img) is not list:
+            # if it isn't a list, make it one
+            img=[img]
+        # then, we are iterate through the list
+        for one_img in img:
+            # We are reading the image binary data
+            img_data = open(one_img, 'rb').read()
+            # We are Attaching the image data to MIMEMultipart
+            # then, using MIMEImage, add the given filename
+            msg.attach(MIMEImage(img_data,
+                                 name=os.path.basename(one_img)))
+        # We also do the same for
+        # attachment 
+    if attachment is not None:
+        # Check whether we have 
+        # lists of attachemnts or not
+        if type(attachment) is not list:
+            # If it isn't a list, make it one
+            attachment = [attachment]
+        # going through for loop
+        for one_attachment in attachment:
+            # opening in reading binary 
+            with open(one_attachment,'rb') as f:
+                # We are Read in the attachemnt
+                # We are using MIMEApplication
+                file = MIMEApplication(
+                    f.read(),
+                    name=os.path.basename(one_attachment)
+                )
+            file['Content-Disposition'] = f'attachmnt;\
+                filename="{os.path.basename(one_attachment)}"'
+            # lastly, we are Adding the attachemnt to our message object
+            msg.attach(file)
+    # We are returning the message   
+    return msg
+
+# defining the main function to call the subfunction
+def main():
+    # We are Calling the message function
+    msg = message("Good", "Hi there!",
+                  r"C:\Users\yuqia\OneDrive\Desktop\6.PNG",
+                  r"C:\Users\yuqia\OneDrive\Desktop\hello.txt")
+    # Make a list of emails, right now is in testing phase, we are only doing one
+    to = ["yuqianlucy@gmail.com"]
+
+    # We arw Providing some data to the sendmail function!
+    smtp.sendmail(from_addr="yuqianlucy@gmail.com",
+                  to_addrs=to, msg=msg.as_string())
     
-    # taking care of exception for different email senting error
-    except Exception as e:
-        # print the Error message
-        print(f"Error sending email: {e}")
+    # Finally, we need to close the connection
+    smtp.quit()
+           
+
+
+
+# import schedule
+# import time
+
+# # Creating an function to send email, which require three parameter, the to_email, the subject, and the body of the email
+# def send_email(to_email, subject, body):
+#     # Need to set variable to store which email it is from
+#     from_email = 'yuqianlucy@gmail.com'
+#     # setting an variable to store the password, kind of worry of security issues, might need to come up a way to fix or modify it
+#     # setting up the server
+#     smtp_server = 'smtp.gmail.com'
+#     # setting up the port 
+#     smtp_port = 587
+
+#     # Setting up an try block to check the server connections
+#     try:
+#         #Create a secure connection to the SMTP server, using the server variable and port variable
+#         server = smtplib.SMTP(smtp_server,smtp_port)
+#         # the starttls function does: (keyfile: str | None = None, certfile: str | None = None, context: SSLContext | None = None) -> _ReplyPuts the connection to the SMTP server into TLS mode.
+#         #If there has been no previous EHLO or HELO command this session, this method tries ESMTP EHLO first.
+#         #If the server supports TLS, this will encrypt the rest of the SMTP session. If you provide the keyfile and certfile parameters, the identity of the SMTP server and client can be checked. This, however, depends on whether the socket module really checks the certificates.
+#         #This method may raise the following exceptions:
+#         #SMTPHeloError The server didn't reply properly tothe helo greeting.
+#         server.starttls()
+#         # We are using server.login function to login to the personal email account
+#         server.login(from_email, password)
+#         # Then, we are composing the email message
+#         message = f'Subject: {subject}\n\n{body}'
+#         # We are sending the email with three parameter from_email, to_email, message
+#         server.sendmail(from_email, to_email, message)
+#         # if Email are senting successfully, then we print out an message to show
+#         print("Email sent successfully!")
     
-    # Take care of the final block
-    finally:
-        server.quit()
+#     # taking care of exception for different email senting error
+#     except Exception as e:
+#         # print the Error message
+#         print(f"Error sending email: {e}")
+    
+#     # Take care of the final block
+#     finally:
+#         server.quit()
+# # Defining the main function
+# def main():
+#     # Defining the require variables
+#     recipient_email = 'yuqianlucy@gmail.com'
+#     reminder_subject = 'Reminder: Follow Up'
+#     reminder_body = 'This is a friendly reminder to follow up on task'
+#     send_email(recipient_email,reminder_subject,reminder_body)
 
-# next, we need to create an function to send the reminder
-def send_reminder():
-    # Creating an variable to store recipent email
-    recipient_email = 'recipient@example.com'
-    # Creating an variable to store reminder_subject
-    reminder_subject = 'Reminder: Follow Up'
-    # Creating an variable to store the reminder body
-    reminder_body = 'This is a friendly reminder to follow up on the task.'
-    # calling the function send_email
-    send_email(recipient_email,reminder_subject,reminder_body)
+# # next, we need to create an function to send the reminder
+# def send_reminder():
+#     # Creating an variable to store recipent email
+#     recipient_email = 'recipient@example.com'
+#     # Creating an variable to store reminder_subject
+#     reminder_subject = 'Reminder: Follow Up'
+#     # Creating an variable to store the reminder body
+#     reminder_body = 'This is a friendly reminder to follow up on the task.'
+#     # calling the function send_email
+#     send_email(recipient_email,reminder_subject,reminder_body)
 
-# Then, we are schedule the reminder to be sent every day at a specific time (9:00 AM)
-schedule.every().day.at("09:00").do(send_reminder)
+# # Then, we are schedule the reminder to be sent every day at a specific time (9:00 AM)
+# schedule.every().day.at("09:00").do(send_reminder)
 
 # Keep the program running to execute schedues tasks
 
+# # Entry point of the program
+# if __name__ =="__main__":
+#     main()
